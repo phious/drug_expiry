f3955cb3ff1710e7db43bcf6d526bf2c3c757143
Yonatan Amare <yonatanamare5000@gmail.com>
2025-05-02 04:12:08 +0300

    ..

--- includes/connect.php
+++ includes/connect.php
@@ -4,7 +4,7 @@ $host = getenv("drug-expiry-db-yonatanamare5000-3761.l.aivencloud.com");
 $username = getenv("avnadmin");
 $password = getenv("AVNS_lyiD-Owz2nkljyhMlV1");
 $database = getenv("defaultdb");
-$conn = mysqli_connect($host,$username,$password,$db);
+$conn = mysqli_connect($host,$username,$password,$database);
 
 if (!$conn) {
 	die("Connection failed: " . mysqli_connect_error());
