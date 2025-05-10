cb985ffbe91a45f6416a650f2e27cfa6505c7a52
Yonatan Amare <yonatanamare5000@gmail.com>
2025-05-10 18:49:34 +0300

    ..

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
-<?php 	session_start();
-		include('header.php'); ?>
+<?php
+require_once 'session.php'; // session_start() is already called here, so no need to call it again
+// Rest of the page code follows
+?>
+<?php 	
+	include('header.php'); ?>
 <body style="background-image: url('image/download.jpg'); background-size:cover;">
 	<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-            <div class="navbar-header">
-				<h1 class="navbar-brand" style="text-align:center;color: #fff;font-size: 30px;margin-left: 450px;text-transform: uppercase;">Drug Expiry Management System For EPSA</h1>
-            </div>
-        </nav>
+        <div class="navbar-header">
+			<h1 class="navbar-brand" style="text-align:center;color: #fff;font-size: 30px;margin-left: 450px;text-transform: uppercase;">Drug Expiry Management System For EPSA</h1>
+        </div>
+    </nav>
 <div class="container">
 	<div id="login_form" class="well">
 		<h2><center><span class="glyphicon glyphicon-lock"></span> Please Login</center></h2>
@@ -21,7 +25,6 @@
 		<div style="color: red; font-size: 15px;">
 			<center>
 			<?php
-				
 				if(isset($_SESSION['msg'])){
 					echo $_SESSION['msg'];
 					unset($_SESSION['msg']);
@@ -29,7 +32,7 @@
 			?>
 			</center>
 		</div>
-</div>
+	</div>
 <?php include('script.php'); ?>
 </body>
 </html>
