2787958db3599da743f778920c7c5c4dfc35762e
Yonatan Amare <yonatanamare5000@gmail.com>
2025-05-10 18:10:15 +0300

    ..

--- admin/session.php
+++ admin/session.php
@@ -1,14 +1,28 @@
 <?php
-	session_start();
-	ob_start();
-	include('../includes/connect.php');
+session_start();
+ob_start();
+include('../includes/connect.php');
 
-	$sq=mysqli_query($conn,"select * from `admin` where id='".$_SESSION['id']."'");
-	$srow=mysqli_fetch_array($sq);
-	
-	$user=$srow['Username'];
-	$password = $srow['Password'];
-	$firstname = $srow['firstname'];
-	$lastname = $srow['lastname'];
-	$phone_number = $srow['phone_number'];
+if (!isset($_SESSION['id']) || empty($_SESSION['id'])) {
+    // redirect if session ID is missing
+    header("Location: ../index.php");
+    exit();
+}
+
+$id = mysqli_real_escape_string($conn, $_SESSION['id']);
+$sq = mysqli_query($conn, "SELECT * FROM `admin` WHERE id='$id'");
+
+if ($sq && mysqli_num_rows($sq) > 0) {
+    $srow = mysqli_fetch_array($sq);
+    $user = $srow['Username'];
+    $password = $srow['Password'];
+    $firstname = $srow['firstname'];
+    $lastname = $srow['lastname'];
+    $phone_number = $srow['phone_number'];
+} else {
+    // destroy session and redirect if user not found
+    session_destroy();
+    header("Location: ../index.php");
+    exit();
+}
 ?>
