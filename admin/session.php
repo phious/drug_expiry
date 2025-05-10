0b7310795832db40a3a84cc8d724f5d1551658f0
Yonatan Amare <yonatanamare5000@gmail.com>
2025-05-10 20:29:32 +0300

    backup

--- admin/session.php
+++ admin/session.php
@@ -1,41 +1,14 @@
 <?php
-// Start session only if none is active
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-    ob_start();
-}
+	session_start();
+	ob_start();
+	include('../includes/connect.php');
 
-include('../includes/connect.php');
-
-// Check if session ID is set and not empty
-if (!isset($_SESSION['id']) || empty($_SESSION['id'])) {
-    header("Location: ../index.php");
-    exit();
-}
-
-// Sanitize session ID
-$admin_id = mysqli_real_escape_string($conn, $_SESSION['id']);
-
-// Query the admin table
-$sq = mysqli_query($conn, "SELECT * FROM `admin` WHERE id = '$admin_id'");
-
-if (!$sq) {
-    error_log("Database query failed: " . mysqli_error($conn));
-    session_destroy();
-    header("Location: ../index.php?error=db_error");
-    exit();
-}
-
-if (mysqli_num_rows($sq) > 0) {
-    $srow = mysqli_fetch_assoc($sq);
-    // Store only necessary user data
-    $user = isset($srow['Username']) ? $srow['Username'] : '';
-    $firstname = isset($srow['firstname']) ? $srow['firstname'] : '';
-    $lastname = isset($srow['lastname']) ? $srow['lastname'] : '';
-    $phone_number = isset($srow['phone_number']) ? $srow['phone_number'] : '';
-} else {
-    session_destroy();
-    header("Location: ../index.php?error=invalid_user");
-    exit();
-}
+	$sq=mysqli_query($conn,"select * from `admin` where id='".$_SESSION['id']."'");
+	$srow=mysqli_fetch_array($sq);
+	
+	$user=$srow['Username'];
+	$password = $srow['Password'];
+	$firstname = $srow['firstname'];
+	$lastname = $srow['lastname'];
+	$phone_number = $srow['phone_number'];
 ?>
--- index.php
+++ index.php
@@ -1,15 +1,11 @@
-<?php
-require_once 'session.php'; // session_start() is already called in session.php
-// Rest of the page code follows
-?>
-<?php 	
-	include('header.php'); ?>
+<?php 	session_start();
+		include('header.php'); ?>
 <body style="background-image: url('image/download.jpg'); background-size:cover;">
 	<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-        <div class="navbar-header">
-			<h1 class="navbar-brand" style="text-align:center;color: #fff;font-size: 30px;margin-left: 450px;text-transform: uppercase;">Drug Expiry Management System For EPSA</h1>
-        </div>
-    </nav>
+            <div class="navbar-header">
+				<h1 class="navbar-brand" style="text-align:center;color: #fff;font-size: 30px;margin-left: 450px;text-transform: uppercase;">Drug Expiry Alert System</h1>
+            </div>
+        </nav>
 <div class="container">
 	<div id="login_form" class="well">
 		<h2><center><span class="glyphicon glyphicon-lock"></span> Please Login</center></h2>
@@ -25,6 +21,7 @@ require_once 'session.php'; // session_start() is already called in session.php
 		<div style="color: red; font-size: 15px;">
 			<center>
 			<?php
+				
 				if(isset($_SESSION['msg'])){
 					echo $_SESSION['msg'];
 					unset($_SESSION['msg']);
@@ -32,7 +29,7 @@ require_once 'session.php'; // session_start() is already called in session.php
 			?>
 			</center>
 		</div>
-	</div>
+</div>
 <?php include('script.php'); ?>
 </body>
 </html>
