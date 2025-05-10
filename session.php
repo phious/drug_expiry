f669b9b16e4e6db261171442de315ba9c78a9960
Yonatan Amare <yonatanamare5000@gmail.com>
2025-05-10 19:49:20 +0300

    ..

--- session.php
+++ session.php
@@ -2,9 +2,7 @@
 // Start session only if none is active
 if (session_status() === PHP_SESSION_NONE) {
     session_start([
-        
-        'cookie_httponly' => true,
-        'use_strict_mode' => true,
+       
     ]);
 }
 
