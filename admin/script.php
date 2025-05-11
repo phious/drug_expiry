05df6b406fe6d59e143141f85c2ea0261ac471be
Yonatan Amare <yonatanamare5000@gmail.com>
2025-05-11 15:58:51 +0300

    updated script.php

--- admin/script.php
+++ admin/script.php
@@ -1,23 +1,30 @@
+<!-- jQuery (only once) -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
-    <script src="../vendor/jquery/jquery.min.js"></script>
-    <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
-	<script src="../dist/js/sb-admin-2.js"></script>
-	<script src="../vendor/metisMenu/metisMenu.min.js"></script>
-    <script src="../vendor/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
-    <script src="../vendor/bootstrap-datepicker/dist/locales/bootstrap-datepicker.de.min.js"></script>
-     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Bootstrap 3 (if you're using .modal(), not Bootstrap 4 classes like modal-dialog-centered) -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
+<!-- Optional: if you're using bootstrap-datepicker -->
+<script src="../vendor/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
 
-	<!-- DataTables JavaScript -->
-    <script src="../vendor/datatables/js/jquery.dataTables.min.js"></script>
-    <script src="../vendor/datatables-plugins/dataTables.bootstrap.min.js"></script>
-    <script src="../vendor/datatables-responsive/dataTables.responsive.js"></script>
-   
-    <script>
-    $(document).ready(function() {
-        $('#dataTables-example').DataTable({
-            responsive: true
-        });
+<!-- Optional: if you're using MetisMenu -->
+<script src="../vendor/metisMenu/metisMenu.min.js"></script>
+
+<!-- Optional: if you're using sb-admin-2.js -->
+<script src="../dist/js/sb-admin-2.js"></script>
+
+<!-- Optional: if you're using DataTables -->
+<script src="../vendor/datatables/js/jquery.dataTables.min.js"></script>
+<script src="../vendor/datatables-plugins/dataTables.bootstrap.min.js"></script>
+<script src="../vendor/datatables-responsive/dataTables.responsive.js"></script>
+
+<!-- Custom JS -->
+<script src="custom.js"></script>
+
+<script>
+$(document).ready(function() {
+    $('#dataTables-example').DataTable({
+        responsive: true
     });
-    </script>
+});
+</script>
